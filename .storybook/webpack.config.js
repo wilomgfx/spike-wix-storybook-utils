e13c3338036141e06f372e60ca63704dff86104b
William Cantin <william.cantin@broadsign.com>
2018-06-13 11:56:29 -0400

    update configs

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,31 +1,20 @@
 const merge = require('lodash/merge');
 const path = require('path');
 const wixStorybookConfig = require('yoshi/config/webpack.config.storybook');
-const wixStorybookUtilsLoader = require('wix-storybook-utils/loader');
 
 module.exports = (config, env, defaultConfig) => {
   const newConfig = wixStorybookConfig(defaultConfig);
 
   return merge(newConfig, {
     context: path.resolve(__dirname, '../src'),
-    externals: {
-      react: 'React',
-      'react-dom': 'ReactDOM'
-    },
-    resolve: {
-      alias: {
-        'wix-style-react': path.resolve(__dirname, '../src'),
-        // 'wix-storybook-utils':  path.resolve(__dirname, '../node_modules/wix-storybook-utils')
-      },
-    },
     module: {
       rules: newConfig.module.rules.concat({
         test: /\.story\.js$/,
-        loader: wixStorybookUtilsLoader,
+        loader: 'wix-storybook-utils/loader',
         options: {
           storyConfig: {
-            moduleName: 'wix-style-react',
-            repoBaseURL: 'https://github.com/wix/wix-style-react/tree/master/src/'
+            moduleName: 'wix-style-react'
+            // repoBaseURL: 'https://github.com/wix/wix-style-react/tree/master/src/'
           }
         }
       })
